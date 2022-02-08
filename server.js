77f50249028185ca67b879bfed4d3503758a253b
raniak92 <93165390+raniak92@users.noreply.github.com>
2022-02-08 07:25:05 +0100

    Add files via upload
    checkpoint express

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+const port = 5000;
+const verifdate = (req, res, next) => {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let hour = new Date().getHours();
+
+  if (
+    days[new Date().getDay()] !== "Saturday" &&
+    days[new Date().getDay()] !== "Sunday" &&
+    hour >= 9 &&
+    hour < 17
+  ) {
+    next();
+  } else {
+    res.send("oops! date not avilable");
+  }
+};
+
+app.use(verifdate);
+
+app.use(express.static("Public"));
+
+app.listen(port, () => {
+  console.log(` ☻ The server is Running in ${port} port`);
+});
